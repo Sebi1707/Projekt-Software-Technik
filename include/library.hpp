3f95da91b5ff8244d994f0db2098fcd7ee781a6c
Sebastian Pflieger <sebastianpflieger@gmail.com>
2024-05-31 11:17:43 +0200

    Festlegung der Struktur für die Lieder

--- include/library.hpp
+++ include/library.hpp
@@ -7,3 +7,13 @@
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
+
+//Festlegung der Struktur für Lieder
+struct Lied{
+    std::string Titel;          //Musiktitel
+    std::string Kuenstler;      //Name des Künstler
+    std::string Album;          //Name des Albums
+    int Erscheinungsjahr;       //Erscheinungsjahr des Liedes; Format "XXXX" z.B. "2024"
+    std::string Genre;          //Genre des Liedes
+    std::string Laenge;         //Länge des Liedes; Format: "MM:SS" z.B. "02:56" für 2 Minuten und 56 Sekunden
+};
